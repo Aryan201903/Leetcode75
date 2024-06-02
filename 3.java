c77e5eda60c290ba45b92b5db44228033b972f1e
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-06-02 20:18:35 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        
+        for (int candy : candies) {
+            if (candy > max) {
+                max = candy;
+            }
+        }
+    
+        List<Boolean> result = new ArrayList<>(candies.length);
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= max);
+        }
+        
+        return result;
+    }
+}
