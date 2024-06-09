690eaf4dd202646f115bc239f0c0eeb412a47d27
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-06-09 20:31:14 +0530

    Created 10.java

---
+++ 10.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int spoint = 0;
+        int tpoint = 0;
+
+        while(spoint < s.length() && tpoint < t.length()){
+            if(s.charAt(spoint) == t.charAt(tpoint)){
+                spoint++;
+            }
+            tpoint++;
+        } 
+
+        return spoint == s.length();
+    }
+}
