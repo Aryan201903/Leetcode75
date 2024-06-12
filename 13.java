6729c054c0c85de0254b58bfa6921adecfee14e8
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-06-12 19:12:33 +0530

    Created 13.java

---
+++ 13.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int maxSum = 0;
+        int sum = 0;
+        for(int i = 0 ; i < k ; i++){
+            sum += nums[i];
+        }   
+        maxSum = sum;
+        int start = 0;
+        int end = k;
+        while(end < nums.length){
+            sum -= nums[start++];
+            sum += nums[end++];
+            maxSum = Math.max(sum , maxSum);
+        }
+
+        return (double)maxSum/k;
+    }
+}
