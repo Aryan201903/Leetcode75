6ba8e95ce92172e79fe57b44566d1e95fa222343
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-06-26 19:26:55 +0530

    Create 16.java

---
+++ 16.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int curr = 0;
+        int prev = 0;
+        int max = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] == 1) curr++;
+            else{
+                max = Math.max(max , prev + curr);
+                prev = curr;
+                curr = 0;
+            }
+        }
+        max = Math.max(max , prev + curr);
+        return max == nums.length ? max - 1 : max;
+    }
+}
