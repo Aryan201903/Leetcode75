44c89ead74b43ec34466957d3f1682c49ce02bb8
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-06-27 19:26:45 +0530

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int[] left = new int[gain.length+1];
+        left[0] = 0;
+        for(int i = 1 ; i < left.length ; i++){
+            left[i] = gain[i-1] + left[i-1];
+        }
+        int max = 0;
+        for(int i = 0 ; i < left.length ; i++){
+            if(left[i] > max){
+                max = left[i];
+            }
+        }
+        return max;
+    }
+}
