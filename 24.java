8f85b471ad536edd93ab30e1d59320332d80edb4
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-07-03 18:03:10 +0530

    Update 24.java

--- 24.java
+++ 24.java
@@ -1,1 +1,28 @@
-
+// Asteroid Collision
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        for(int i : asteroids){
+            boolean destroyed = false;
+            while(!st.isEmpty() && i < 0 && st.peek() > 0){
+                if(st.peek() < -i){
+                    st.pop();
+                    continue;
+                }
+                else if(st.peek() == -i){
+                    st.pop();
+                }
+                destroyed = true;
+                break;
+            }
+            if(!destroyed){
+                st.push(i);
+            }
+        }
+        int[] result = new int[st.size()];
+        for(int i = result.length-1 ; i >= 0 ; i--){
+            result[i] = st.pop();
+        }
+        return result;
+    }
+}
