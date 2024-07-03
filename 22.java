a814cef171dc9414c520f7d0a70d2d056acda1b8
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-07-03 17:59:56 +0530

    Update 22.java

--- 22.java
+++ 22.java
@@ -1,1 +1,37 @@
+// Removing Stars From a String
+class Solution {
+    public String removeStars(String s) {
+        char[] arr = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (arr[i] == '*') {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(arr[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
 
+
+// class Solution {
+//     public String removeStars(String s) {
+//         Stack<Character> st = new Stack<>();
+//         char[] arr = s.toCharArray();
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             if (arr[i] == '*') {
+//                 st.pop();
+//             } else {
+//                 st.push(arr[i]);
+//             }
+//         }
+        
+//         StringBuilder result = new StringBuilder();
+//         while (!st.isEmpty()) {
+//             result.insert(0, st.pop());
+//         }
+        
+//         return result.toString();
+//     }
